e4cddd9647370934ce8af15e04aa75092a1f0982
RamiroMartinezSantos <RamiroMartinezSantos@users.noreply.github.com>
2023-06-15 00:49:15 -0600

    Subido

--- src/IngresarAlumno.java
+++ src/IngresarAlumno.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 public class IngresarAlumno {
     public ArrayList<Alumno> alumnos;
     public IngresarAlumno(){alumnos = new ArrayList<>();}
-    public void ingresar(ArrayList<Alumno> alumnos,String nombre,String apellido,String telefono,String ine){
-        alumnos.add(new Alumno(nombre,apellido,telefono,ine));
+    public void ingresar(ArrayList<Alumno> alumnos,String nombre,String apellidos,String telefono,String ine,String estado){
+        alumnos.add(new Alumno(nombre,apellidos,telefono,ine,estado));
     }
     public ArrayList<Alumno> getAlumnos() {return alumnos;}
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
     public static void main(String[] args) {
-        Formulario formulario = new Formulario();
+        FormComboBox formComboBox = new FormComboBox();
     }
 }
